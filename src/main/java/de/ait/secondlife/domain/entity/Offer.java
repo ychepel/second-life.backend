8aecf99b91cd23dbb4ff6fd1ad6492e2127cb514
Ruslan Senkin <rsenkin24@gmail.com>
2024-05-21 21:51:32 +0300

    fix: bag uuid generation

---
+++ src/main/java/de/ait/secondlife/domain/entity/Offer.java
@@ -0,0 +1,21 @@
+//package de.ait.secondlife.domain.entity;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Table(name = "offer")
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Data
+//@Builder
+//public class Offer {
+//
+//
+//
+//
+//}
