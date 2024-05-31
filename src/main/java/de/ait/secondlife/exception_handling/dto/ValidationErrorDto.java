0cbd700f9c2c0322ce1080f383b093686fcb9840
Algalon1 <kirill.bernatskyi@developex.net>
2024-05-31 23:43:16 +0200

    Changed ValidationErrorDto as it was before last changes

--- src/main/java/de/ait/secondlife/exception_handling/dto/ValidationErrorDto.java
+++ src/main/java/de/ait/secondlife/exception_handling/dto/ValidationErrorDto.java
@@ -13,12 +13,12 @@ import lombok.NoArgsConstructor;
 @Schema(name = "Validation Error", description = "Validation error description")
 public class ValidationErrorDto {
 
-    @Schema(description = "name of the field in which the error occurred", example = "some field name")
+    @Schema(description = "name of the field in which the error occurred", example = "email")
     private String field;
 
-    @Schema(description = "the value entered by the user and which was rejected by the server", example = "some value of the field")
+    @Schema(description = "the value entered by the user and which was rejected by the server", example = "some.email@server.com")
     private String rejectedValue;
 
-    @Schema(description = "the message we need to show to the user", example = "error description")
+    @Schema(description = "the message we need to show to the user", example = "must be valid email format")
     private String message;
 }
