84edf8604d279179108c32cca91f34750ef19094
Yuriy Chepel <yuriy.chepel@gmail.com>
2024-06-03 03:22:02 +0200

    updated cors configuration

--- src/main/java/de/ait/secondlife/config/WebConfig.java
+++ src/main/java/de/ait/secondlife/config/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
             }
