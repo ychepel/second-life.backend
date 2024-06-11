e3f6e2a3788e471214406e66be876227cffe0d46
Ruslan Senkin <rsenkin24@gmail.com>
2024-06-11 19:04:45 +0300

    fix: add test

--- src/main/java/de/ait/secondlife/domain/dto/ImageRequestDto.java
+++ src/main/java/de/ait/secondlife/domain/dto/ImageRequestDto.java
@@ -1,13 +1,16 @@
 package de.ait.secondlife.domain.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @Schema(description = "Image request dto")
+@AllArgsConstructor
 public class ImageRequestDto {
     @Schema(
             description = "Full name of image file",
             example = "https://second-life.fra1.digitaloceanspaces.com\\prod\\offer\\1\\1024x1024_91dcf1db-b934-479a-828f-59b46a522a5a.jpg"
-    ) private String fileName;
+    )
+    private String fileName;
 }
--- src/test/java/de/ait/secondlife/controllers/ImagesIntegrationTest.java
+++ src/test/java/de/ait/secondlife/controllers/ImagesIntegrationTest.java
@@ -1,8 +1,10 @@
 package de.ait.secondlife.controllers;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.ait.secondlife.constants.EntityType;
+import de.ait.secondlife.domain.dto.ImageRequestDto;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,7 +14,7 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -25,16 +27,18 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Map;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @DisplayName("Image integration tests:")
 @DisplayNameGeneration(value = DisplayNameGenerator.ReplaceUnderscores.class)
+@Transactional
+@Rollback
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ImagesIntegrationTest {
 
     @Autowired
@@ -44,8 +48,8 @@ public class ImagesIntegrationTest {
     Long createdOfferId;
     private String token;
 
-    @BeforeEach
-    public void setAdminToken() throws Exception {
+    @BeforeAll
+    public void setToken() throws Exception {
         MvcResult authResult = mockMvc.perform(post("/v1/auth/user/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("""
@@ -59,40 +63,33 @@ public class ImagesIntegrationTest {
         Map<String, String> authMap = mapper.readValue(authResponse, new TypeReference<>() {
         });
         token = authMap.get(("accessToken"));
+
+        MvcResult result = mockMvc.perform(post("/v1/offers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + token)
+                        .content("""
+                                  {
+                                  "title": "1newoffer",
+                                  "description": "1asfdfadsgdgfgdfgasdasdadadfgdfgdfgdgdag1",
+                                  "auctionDurationDays": 3,
+                                  "startPrice": 2000,
+                                  "step": 4,
+                                  "winBid": 90,
+                                  "isFree":false,
+                                  "userId": 1,
+                                  "categoryId": 3,
+                                  "locationId": 2
+                                }"""))
+                .andExpect(status().isCreated())
+                .andReturn();
+        String response = result.getResponse().getContentAsString();
+        createdOfferId = mapper.readTree(response).get("id").asLong();
     }
 
     @Nested
-    @Transactional
-    @Rollback
     @DisplayName("POST /v1/images/upload")
     public class UploadImageTest {
-
-        @BeforeEach
-        public void setup() throws Exception {
-            MvcResult result = mockMvc.perform(post("/v1/offers")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .header("Authorization", "Bearer " + token)
-                            .content("""
-                                      {
-                                      "title": "1newoffer",
-                                      "description": "1asfdfadsgdgfgdfgasdasdadadfgdfgdfgdgdag1",
-                                      "auctionDurationDays": 3,
-                                      "startPrice": 2000,
-                                      "step": 4,
-                                      "winBid": 90,
-                                      "isFree":false,
-                                      "userId": 1,
-                                      "categoryId": 3,
-                                      "locationId": 2
-                                    }"""))
-                    .andExpect(status().isCreated())
-                    .andReturn();
-            String response = result.getResponse().getContentAsString();
-            createdOfferId = mapper.readTree(response).get("id").asLong();
-        }
-
         @Test
-        @Order(1)
         public void set_image_for_offer() throws Exception {
 
             File imageFile = ResourceUtils.getFile("classpath:test_image/testImg.jpeg");
@@ -121,11 +118,68 @@ public class ImagesIntegrationTest {
                     .andExpect(status().isOk())
                     .andExpect(jsonPath("$.message", is("Image(s) successful saved")));
         }
-
-        @Test
-        @Order(2)
-        public void del_image_for_offer() throws Exception {
-
-        }
     }
+
+//    @Nested
+//    @DisplayName("DELETE /v1/images")
+//     public class DeleteImageTest {
+//
+//        String fileName;
+//
+//        @BeforeEach
+//        public void setup() throws Exception {
+//            File imageFile = ResourceUtils.getFile("classpath:test_image/testImg.jpeg");
+//            byte[] imageBytes = null;
+//            try (FileInputStream fis = new FileInputStream(imageFile)) {
+//                imageBytes = fis.readAllBytes();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            MockMultipartFile testFile = new MockMultipartFile(
+//                    "file",
+//                    imageFile.getName(),
+//                    MediaType.IMAGE_JPEG_VALUE,
+//                    imageBytes
+//            );
+//            MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+//            params.add("entityType", EntityType.OFFER.getType());
+//            params.add("entityId", String.valueOf(createdOfferId));
+//
+//            mockMvc.perform(multipart("/v1/images/upload")
+//                            .file(testFile)
+//                            .params(params)
+//                            .header("Authorization", "Bearer " + token)
+//                            .contentType(MediaType.MULTIPART_FORM_DATA)
+//                    )
+//                    .andExpect(status().isOk());
+//
+//
+//            MvcResult result = mockMvc.perform(get("/v1/offers/" + createdOfferId)
+//                            .contentType(MediaType.APPLICATION_JSON))
+//                    .andExpect(status().isOk())
+//                    .andReturn();
+//            String response = result.getResponse().getContentAsString();
+//
+//            fileName = mapper.readTree(response)
+//                    .get("images")
+//                    .get("images")
+//                    .get("0")
+//                    .get("320x320").toString();
+//
+//            fileName = fileName.replaceAll("^\"|\"$", "");
+//        }
+//
+//        @Test
+//        public void delete_image_for_offer() throws Exception {
+//            ImageRequestDto imageRequestDto = new ImageRequestDto(fileName.replaceAll("\"", ""));
+//
+//            String json = mapper.writeValueAsString(imageRequestDto);
+//            mockMvc.perform(delete("/v1/images")
+//                            .contentType(MediaType.APPLICATION_JSON)
+//                            .header("Authorization", "Bearer " + token)
+//                            .content(json))
+//                    .andExpect(status().isOk())
+//                    .andExpect(jsonPath("$.message", is("Image deleted successfully")));
+//        }
+//    }
 }
